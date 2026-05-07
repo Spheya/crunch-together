6368c6f0f0ea9ef62c4d54ab436a4b2470dc8cde
Spheya <tamara@spheya.com>
2026-05-07 14:25:22 +0200

    cleaning up

--- script.ts
+++ script.ts
@@ -8,21 +8,23 @@ const roomSectionTemplate = `
     </div>
 `
 
+const events = [ "playing", "pause", "seeked", "ratechange" ]
+
+let videoPlayer : HTMLVideoElement | null = null
 let socket : Socket | undefined
 let roomUrl : string = ''
-let synced : boolean = false;
+let skipEvent : { [index: string]: number } = {}
 
 function connect(room?: string) {
     socket = io('https://crunch-together.onrender.com/')
     socket.on('connect', () => {
+        console.log('[Crunch Together] connection opened')
         if(room) {
             socket?.emit('join-room', room)
         } else {
             socket?.emit('create-room', window.location.href)
         }
     })
-
-    socket.on('disconnect', disconnect)
 
     socket.on('room-joined', (link: string) => {
         roomUrl = link
@@ -31,22 +33,20 @@ function connect(room?: string) {
         if(room) {
             socket?.emit('get-state')
         } else {
-            let videoPlayer = document.querySelector('.bitmovinplayer-container video')
-            synced = true;
-            if(videoPlayer && videoPlayer instanceof HTMLVideoElement) onStateUpdate(videoPlayer)
+            if(videoPlayer) onStateUpdate('roomcreation', videoPlayer)
         }
     })
 
-    socket.on('state-update', (state: VideoState) => { 
-        syncVideoPlayer(state)
-        console.log("synced!")
-    })
+    socket.on('state-update', (state: VideoState) => syncVideoPlayer(state))
+    
+    socket.on('disconnect', disconnect)
 
     let buttonContent = document.querySelector('.crunch-together-button span')
     if(buttonContent) buttonContent.textContent = 'Leave Room'
 }
 
 function disconnect() {
+    console.log('[Crunch Together] connection closed')
     socket?.close()
     socket = undefined
     roomUrl = ''
@@ -92,61 +92,76 @@ function createButton(header : Element) {
     }
 }
 
-function onEpisodeLoad() {
-    let header = document.querySelector('.current-media-header')
-    if(header) { 
-        createButton(header)
-        if(roomUrl !== '') createRoomInfo(header)
-    }
+function isPlaying() {
+    if(!videoPlayer) return false
+    return videoPlayer.currentTime > 0 && !videoPlayer.paused && !videoPlayer.ended && videoPlayer.readyState > 2
 }
 
 function getVideoState(videoPlayer: HTMLVideoElement) : VideoState {
     return { 
         url: window.location.href,
-        playing: videoPlayer.currentTime > 0 && !videoPlayer.paused && !videoPlayer.ended && videoPlayer.readyState > 2,
+        playing: isPlaying(),
         time: videoPlayer.currentTime,
         rate: videoPlayer.playbackRate
     }
 }
 
-function onStateUpdate(videoPlayer: HTMLVideoElement) {
-    console.log(`state change detected synced=${synced}`)
-    if(synced && socket) { 
-        console.log("video player state change")
+async function syncVideoPlayer(targetState: VideoState) {
+    if(videoPlayer) {
+        console.log('[Crunch Together] synchronizing video player')
+
+        if(!targetState.playing && isPlaying()) {
+            skipEvent['pause'] = (skipEvent['pause'] || 0) + 1
+            videoPlayer.pause()
+        }
+
+        if(videoPlayer.playbackRate != targetState.rate) {
+            skipEvent['ratechange'] = (skipEvent['ratechange'] || 0) + 1
+            videoPlayer.playbackRate = targetState.rate
+        }
+        
+        if(Math.abs(videoPlayer.currentTime - targetState.time) > 0.5) {
+            skipEvent['seeked'] = (skipEvent['seeked'] || 0) + 1
+            videoPlayer.currentTime = targetState.time
+        }
+
+        if(targetState.playing && !isPlaying()) {
+            skipEvent['playing'] = (skipEvent['playing'] || 0) + 1
+            await videoPlayer.play().catch(() => {})
+        }
+    }
+}
+
+function onStateUpdate(event: string, videoPlayer: HTMLVideoElement) {
+    if(skipEvent[event]) {
+        --skipEvent[event]
+        return
+    }
+
+    if(socket) { 
+        console.log(`[Crunch Together] player state update: ${event}`)
         socket.emit('state-update', getVideoState(videoPlayer))
     }
 }
 
-function syncVideoPlayer(targetState: VideoState) {
-    let videoPlayer = document.querySelector('.bitmovinplayer-container video')
-    if(videoPlayer && videoPlayer instanceof HTMLVideoElement) {
-        synced = false
-        videoPlayer.currentTime = targetState.time
-        videoPlayer.playbackRate = targetState.rate
-        if(targetState.playing) {
-            videoPlayer.play()
-        } else {
-            videoPlayer.pause()
-        }
-        setTimeout(() => synced = true, 1000)
-    }
-}
-
 function onVideoPlayerLoad() {
-    if(!socket) {
-        // Connect if we have a room URL
+    if(videoPlayer) { // Only set up everything if there's actually a video player on the page (in case of a false positive calling this function)
+        // Immediately connect if the URL contains a room code
         let room = new URLSearchParams(window.location.search).get('ctroom')
         if(room) connect(room)
-    }
 
-    let videoPlayer = document.querySelector('.bitmovinplayer-container video')
-    if(videoPlayer && videoPlayer instanceof HTMLVideoElement) {
-        videoPlayer.addEventListener("playing", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
-        videoPlayer.addEventListener("pause", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
-        videoPlayer.addEventListener("ended", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
-        videoPlayer.addEventListener("seeked", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
-        videoPlayer.addEventListener("ratechange", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
-        videoPlayer.addEventListener("waiting", () => { onStateUpdate(videoPlayer) }, { capture: false, passive: true });
+        // Create the crunch-together UI
+        let header = document.querySelector('.current-media-header')
+        if(header) { 
+            createButton(header)
+            if(roomUrl !== '') createRoomInfo(header)
+        }
+
+        // Setup video player events
+        for(let event of events) {
+            skipEvent[event] = 0;
+            videoPlayer.addEventListener(event, () => { if(videoPlayer) onStateUpdate(event, videoPlayer) }, { capture: false, passive: true })
+        }
     }
 }
 
@@ -155,9 +170,12 @@ function onVideoPlayerLoad() {
 let observer = new MutationObserver((mutations) => {
     for(let mutation of mutations) {
         for(let node of mutation.addedNodes) {
-            if(!(node instanceof Element)) continue;            
-            if(node.classList?.contains('erc-watch-episode')) onEpisodeLoad(); // erc-watch-episode contains the episode description
-            if(node.classList?.contains('kat:absolute')) onVideoPlayerLoad(); // I don't fully understand why this works, but the video player doesnt exist before this
+            if(!(node instanceof Element)) continue
+
+            // there might be a better way to do this, but this is very robust and will set the videoplayer to null when it gets removed
+            let hasVideoPlayer = videoPlayer != null;
+            videoPlayer = document.querySelector('.bitmovinplayer-container video')
+            if(videoPlayer && !hasVideoPlayer) onVideoPlayerLoad()
         }
     }
 })
